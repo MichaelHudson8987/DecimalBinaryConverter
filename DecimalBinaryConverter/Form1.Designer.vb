a7bf9feea571520a501ce32bf74010bb589115a6
MichaelHudson <158861154+MichaelHudson8987@users.noreply.github.com>
2024-11-27 20:14:01 -0400

    Complete project

---
+++ DecimalBinaryConverter/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblDecimalTitle = New System.Windows.Forms.Label()
+        Me.txtDecimal = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblBinaryTitle = New System.Windows.Forms.Label()
+        Me.lblBinaryOutput = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.txtBinary = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(48, 33)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(285, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP Decimal to Binary Converter"
+        '
+        'lblDecimalTitle
+        '
+        Me.lblDecimalTitle.AutoSize = True
+        Me.lblDecimalTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDecimalTitle.Location = New System.Drawing.Point(151, 97)
+        Me.lblDecimalTitle.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblDecimalTitle.Name = "lblDecimalTitle"
+        Me.lblDecimalTitle.Size = New System.Drawing.Size(52, 13)
+        Me.lblDecimalTitle.TabIndex = 1
+        Me.lblDecimalTitle.Text = "Decimal"
+        '
+        'txtDecimal
+        '
+        Me.txtDecimal.Location = New System.Drawing.Point(154, 113)
+        Me.txtDecimal.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.txtDecimal.Name = "txtDecimal"
+        Me.txtDecimal.Size = New System.Drawing.Size(206, 20)
+        Me.txtDecimal.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(34, 153)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 27)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(34, 195)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(70, 27)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'lblBinaryTitle
+        '
+        Me.lblBinaryTitle.AutoSize = True
+        Me.lblBinaryTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBinaryTitle.Location = New System.Drawing.Point(151, 168)
+        Me.lblBinaryTitle.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblBinaryTitle.Name = "lblBinaryTitle"
+        Me.lblBinaryTitle.Size = New System.Drawing.Size(42, 13)
+        Me.lblBinaryTitle.TabIndex = 5
+        Me.lblBinaryTitle.Text = "Binary"
+        '
+        'lblBinaryOutput
+        '
+        Me.lblBinaryOutput.AutoSize = True
+        Me.lblBinaryOutput.Location = New System.Drawing.Point(126, 207)
+        Me.lblBinaryOutput.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblBinaryOutput.Name = "lblBinaryOutput"
+        Me.lblBinaryOutput.Size = New System.Drawing.Size(0, 15)
+        Me.lblBinaryOutput.TabIndex = 6
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 118)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(121, 19)
+        Me.RadioButton1.TabIndex = 7
+        Me.RadioButton1.Text = "Binary to Decimal"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Checked = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 93)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(121, 19)
+        Me.RadioButton2.TabIndex = 8
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Decimal to Binary"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'txtBinary
+        '
+        Me.txtBinary.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBinary.Location = New System.Drawing.Point(154, 184)
+        Me.txtBinary.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.txtBinary.Name = "txtBinary"
+        Me.txtBinary.ReadOnly = True
+        Me.txtBinary.Size = New System.Drawing.Size(206, 20)
+        Me.txtBinary.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(381, 245)
+        Me.Controls.Add(Me.txtBinary)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.lblBinaryOutput)
+        Me.Controls.Add(Me.lblBinaryTitle)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtDecimal)
+        Me.Controls.Add(Me.lblDecimalTitle)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.Text = "IP Decimal to Binary Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblDecimalTitle As Label
+    Friend WithEvents txtDecimal As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents lblBinaryTitle As Label
+    Friend WithEvents lblBinaryOutput As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents txtBinary As TextBox
+End Class
---
+++ DecimalBinaryConverter/Form1.vb
@@ -0,0 +1,244 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+
+    '#Calculate button click#
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FormatValidate As String = ""
+        Dim NumericTest As String
+        Dim UserInput As String()
+        Dim ConversionOutput As String = ""
+        Dim BitNumber As Integer = 128
+        Dim DecimalSection As Integer = 0
+        Dim BinarySection As Integer = 0
+        Dim validBinary As Integer = 1
+        Dim Counter As Integer
+        Dim Counter2 As Integer
+
+        '#Decimal to binary#
+        If txtBinary.ReadOnly = True Then
+
+            NumericTest = Replace(txtDecimal.Text, ".", "0") 'Replaces any periods in input with numbers for IsNumeric verification
+            UserInput = Split(txtDecimal.Text, ".") 'Splits input into an array of substrings (decimal sections), separated by delimiter (period)
+
+            '#Validation#
+            'Validating input only contains numbers
+            If Not IsNumeric(NumericTest) Then
+                MsgBox("IP decimal is invalid. Must contain only numbers and periods")
+                txtDecimal.Text = ""
+
+                'Validating that input has been split into an array of 4 substrings (decimal sections)
+            ElseIf UserInput.Count = 4 Then
+
+                For Counter2 = 0 To 3
+
+                    'If any decimal sections are less than 3 digits long, add 1 or 2 "0"s to force format conformity
+                    'Each decimal section is then combined to one validation string
+                    If UserInput(Counter2).Length = 1 Then
+                        FormatValidate = FormatValidate + "00" + UserInput(Counter2)
+
+                    ElseIf UserInput(Counter2).Length = 2 Then
+                        FormatValidate = FormatValidate + "0" + UserInput(Counter2)
+
+                    ElseIf UserInput(Counter2).Length = 3 Then
+                        FormatValidate += UserInput(Counter2)
+
+                    End If
+
+                    'Adding period to the validation string
+                    If Counter2 < 3 Then
+                        FormatValidate += "."
+
+                    End If
+
+                Next
+
+                'Validating that validation string follows format
+                If Not FormatValidate Like ("###.###.###.###") Then
+                    MsgBox("IP decimal is invalid. Must follow format: #.#.#.#")
+                    txtDecimal.Text = ""
+
+                    'Validating decimal sections aren't higher than 255
+                ElseIf ((UserInput(0) Or UserInput(1) Or UserInput(2) Or UserInput(3)) > 255) Then
+                    MsgBox("IP decimal is invalid. Values must be 255 or lower")
+                    txtDecimal.Text = ""
+
+                Else
+                    DecimalSection = UserInput(Counter)
+
+                    '#Converting decimal input to binary#
+                    For Counter = 0 To 3
+
+                        'Adding "." to output in between each conversion. 
+                        If (Counter > 0) And (Counter < 4) Then
+                            ConversionOutput += "."
+
+                            'Resetting decimal section to counter number after every loop
+                            DecimalSection = UserInput(Counter)
+
+                        End If
+
+                        'Resetting bit number to 128 so conversion can be repeated
+                        BitNumber = 128
+
+                        'Add all 0's to output if decimal section is 0
+                        If DecimalSection = 0 Then
+                            ConversionOutput += "00000000"
+
+                        End If
+
+                        'Repeats until decimal section equals 0
+                        While DecimalSection > 0
+
+                            'If decimal section is HIGHER or EQUAL to bit number, "1" is added to output string and bit number is subtracted from decimal. Bit number is then divided by 2
+                            If DecimalSection >= BitNumber Then
+                                ConversionOutput += "1"
+                                DecimalSection -= BitNumber
+                                BitNumber /= 2
+
+                                'Adds remaining "0"s to output string if decimal section already equals 0
+                                If DecimalSection = 0 Then
+
+                                    While BitNumber > 0
+                                        ConversionOutput +=
+"0"
+                                        BitNumber /= 2
+
+                                    End While
+
+                                End If
+
+                            Else
+                                'If decimal section is LOWER than bit number, "0" is added to output string and bit number is subtracted from decimal. Bit number is then divided by 2
+                                ConversionOutput += "0" '
+                                BitNumber /= 2
+
+                            End If
+                        End While
+                    Next
+                End If
+            Else
+                'If array is not 4, display error message
+                MsgBox("IP address is invalid. Must follow format: #.#.#.#")
+                txtDecimal.Text = ""
+
+            End If
+
+            'Outputting binary output string to text box
+            txtBinary.Text = ConversionOutput
+
+            '#Binary to decimal#
+        ElseIf txtDecimal.ReadOnly = True Then
+            NumericTest = Replace(txtBinary.Text, ".", "0")
+            UserInput = Split(txtBinary.Text, ".")
+            FormatValidate = txtBinary.Text
+
+            '#Validation#
+            'Validating input only contains numbers
+            If Not IsNumeric(NumericTest) Then
+                MsgBox("Binary input is invalid. Must contain only numbers and periods")
+                txtBinary.Text = ""
+                txtDecimal.Text = ""
+                validBinary = 0
+
+                'Validating input is in an array of 4 substrings (binary sections)
+            ElseIf UserInput.Count = 4 Then
+                If Not FormatValidate Like ("########.########.########.########") Then
+                    MsgBox("Binary input is invalid. Must follow format: ########.########.########.########")
+                    txtBinary.Text = ""
+                    txtDecimal.Text = ""
+                    validBinary = 0
+
+                    'Validating input contains only "0", "1", or "."
+                Else
+                    For IntCount = 0 To FormatValidate.Length - 1
+                        If FormatValidate(IntCount) = "0" Or FormatValidate(IntCount) = "1" Or FormatValidate(IntCount) = "." Then
+                            Continue For
+
+                        Else
+                            MsgBox("Binary input is invalid. Must contain only 1's, 0's, and periods.")
+                            txtBinary.Text = ""
+                            txtDecimal.Text = ""
+                            validBinary = 0
+                            Exit For
+
+                        End If
+                    Next
+                End If
+
+                'Outputting error if input is invalid
+            Else
+                MsgBox("Binary input is invalid. Must follow format: ########.########.########.########")
+                txtBinary.Text = ""
+                txtDecimal.Text = ""
+                validBinary = 0
+
+            End If
+
+            'Resetting bit number
+            BitNumber = 128
+
+            'Adding bit number value to binary section if position in string is "1", nothing if "0". If position is ".", binary section is added
+            'to conversion output. Bitnumber is divided by 2 after every loop.
+            For IntCount = 0 To FormatValidate.Length - 1
+                If FormatValidate(IntCount) = "1" Then
+                    BinarySection += BitNumber
+                    BitNumber /= 2
+
+                ElseIf FormatValidate(IntCount) = "0" Then
+                    BitNumber /= 2
+
+                ElseIf FormatValidate(IntCount) = "." Then
+                    ConversionOutput += CStr(BinarySection)
+                    ConversionOutput += "."
+                    BitNumber = 128
+                    BinarySection = 0
+
+                End If
+
+            Next
+
+            'Outputting last binary section to output string
+            ConversionOutput += CStr(BinarySection)
+
+            'Outputting conversion string to decimal textbox if binary input is valid
+            If validBinary = 1 Then
+                txtDecimal.Text = ConversionOutput
+
+            End If
+        End If
+    End Sub
+
+    '#Clear button click#
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtDecimal.Text = ""
+        txtBinary.Text = ""
+    End Sub
+
+    '#Enter equals Calculate button press (decimal)#
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtDecimal.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Button1_Click(Me, EventArgs.Empty)
+        End If
+    End Sub
+
+    '#Enter equals Calculate button press (binary)#
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtBinary.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Button1_Click(Me, EventArgs.Empty)
+        End If
+    End Sub
+    '#Binary to decimal button pressed#
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        txtDecimal.ReadOnly = True
+        txtBinary.ReadOnly = False
+        txtDecimal.Text = ""
+    End Sub
+
+    '#Decimal to binary button pressed#
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        txtDecimal.ReadOnly = False
+        txtBinary.ReadOnly = True
+        txtBinary.Text = ""
+    End Sub
+End Class
